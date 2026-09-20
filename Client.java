8dc5b8add979dea37e2e0298dde8b1007e68f5a6
JuanmaDevG <juanmagonzalezmartinez@protonmail.com>
2026-09-20 22:48:27 +0200

    Completely functional command server

--- Client.java
+++ Client.java
@@ -1,9 +1,11 @@
 import java.net.Socket;
 import java.net.SocketAddress;
+import java.net.SocketTimeoutException;
 import java.net.UnknownHostException;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.net.ConnectException;
 import java.net.InetSocketAddress;
 import java.util.Scanner;
 
@@ -15,10 +17,11 @@ public class Client {
       <command> <arg1> [arg2] ...
 
       Options:
-      h || help             -- prints this message
-      sum <int1> <int2>     -- sum numbers""";
+      h || help                       -- prints this message
+      q || quit                       -- Close connection and exit
+      sum <int1> <int2> [int3] ...    -- sum numbers\n""";
 
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) {
     Socket socket = new Socket();
 
     while (!socket.isConnected()) {
@@ -26,16 +29,33 @@ public class Client {
         socket.connect(addr, TIMEOUT_MS);
         break;
       } catch (UnknownHostException uhex) {
+        System.err.println("-- DNS failed, the server could not be resolved --");
+        return;
+
+      } catch (SocketTimeoutException ex) {
         System.err.println("-- Server unavailable, trying again --");
+        socket = new Socket();
+
+      } catch (ConnectException coex) {
+        System.err.println("-- Server unavailable, trying again --");
+        try {
+          Thread.sleep(TIMEOUT_MS);
+        } catch (InterruptedException iex) {
+          System.err.println("-- Wait timer to retry got interrupted, aborted --");
+          return;
+        }
+        socket = new Socket();
+
+      } catch (IOException ioex) {
+        System.out.println("-- Unavailable to open socket --");
+        return;
       }
     }
-    String s = """
-        hello
-        nigger""";
     System.out.println("-- Connected, welcome to the server command line --");
     System.out.println(help_msg);
 
-    try (
+    final Socket sk = socket; // captured pointer to be closeable
+    try (sk;
         DataInputStream in = new DataInputStream(socket.getInputStream());
         DataOutputStream out = new DataOutputStream(socket.getOutputStream());
         Scanner sc = new Scanner(System.in);) {
@@ -43,18 +63,23 @@ public class Client {
       while (true) {
         System.out.print("$ ");
         String input = sc.nextLine();
-        if (input.equals("h") || input.equals("help")) {
-          System.out.println(help_msg);
-          continue;
+        switch (input) {
+          case "h":
+          case "help":
+            System.out.println(help_msg);
+            continue;
+          case "":
+            continue;
+          default:
+            out.writeUTF(input);
+            input = in.readUTF();
+            System.out.println(input);
         }
-
-        out.writeUTF(input);
-        input = in.readUTF();
-        System.out.println(input);
       }
 
     } catch (IOException ioex) {
-      System.err.println(ioex.getMessage());
+      if (ioex.getMessage() != null)
+        System.err.println(ioex.getMessage());
     }
   }
 }
--- Server.java
+++ Server.java
@@ -11,24 +11,33 @@ import java.lang.SecurityException;
 
 class CommandException extends Exception {
   private InetAddress addr;
+  private String command;
 
-  public CommandException(InetAddress addr, String msg) {
+  public CommandException(InetAddress addr, String msg, String command) {
     super(msg);
     this.addr = addr;
+    this.command = command;
   }
 
   @Override
   public String getMessage() {
-    return ""; // TODO: no response to this.addr: super.getMessage()
+    return "%s: %s (user sent %s)".formatted(addr, super.getMessage(), command);
+  }
+
+  public String getClientMessage() {
+    return "-- Request failed -- %s".formatted(super.getMessage());
   }
 }
 
 public class Server {
   public static void main(String[] args) throws IOException {
     try (ServerSocket sv = new ServerSocket(5000);) {
-      Socket sk = sv.accept();
-      Thread response = new Thread(() -> computeCommand(sk));
-      response.start();
+      while (true) {
+        Socket sk = sv.accept();
+        System.out.printf("Accepted %s session\n", sk.getInetAddress());
+        Thread response = new Thread(() -> computeCommand(sk));
+        response.start();
+      }
     } catch (IOException | SecurityException ex) {
       System.out.println(ex.getMessage());
     }
@@ -39,19 +48,49 @@ public class Server {
         DataInputStream in = new DataInputStream(sk.getInputStream());
         DataOutputStream out = new DataOutputStream(sk.getOutputStream());) {
 
-      String[] command = in.readUTF().split(" +"); // Split, no spaces
-      System.out.printf("%s requested %s: ", sk.getInetAddress().toString(), command[0]);
-      switch (command[0]) {
-        case "sum":
-          if (command.length != 3) { // TODO: support for more than 2 operands
-          }
+      while (!sk.isClosed()) {
+        try {
+          StringBuilder sb = new StringBuilder();
+          String[] command = in.readUTF().split(" +"); // Split, no spaces
+          System.out.printf("%s requested command %s\n", sk.getInetAddress().toString(), command[0]);
+          switch (command[0]) {
+            case "sum":
+              if (command.length < 3) {
+                throw new CommandException(
+                    sk.getInetAddress(),
+                    "not enough operands",
+                    command[0]);
+              }
+              try {
+                int result = Integer.parseInt(command[1]);
+                sb.append(command[1]);
+                for (int i = 2; i < command.length; i++) {
+                  sb.append(" + ");
+                  sb.append(command[i]);
+                  result += Integer.parseInt(command[i]);
+                }
+                sb.append(" = ");
+                sb.append(result);
 
-          int x = Integer.parseInt(command[1]), y = Integer.parseInt(command[2]), r = x + y;
-          System.out.printf("%i + %i = %i", x, y, r);
-          out.writeUTF(Integer.toString(r));
-          break;
-        default:
-      } // TODO: catch CommandException with custom message
+                System.out.println(sb.toString());
+                out.writeUTF(Integer.toString(result));
+              } catch (NumberFormatException fmtex) {
+                throw new CommandException(sk.getInetAddress(), "required only numbers", command[0]);
+              }
+              break;
+            case "q":
+            case "quit":
+              sk.close();
+              return;
+            default:
+              throw new CommandException(
+                  sk.getInetAddress(), "this does not exist", command[0]);
+          }
+        } catch (CommandException coex) {
+          out.writeUTF(coex.getClientMessage());
+          System.err.println(coex.getMessage());
+        }
+      }
 
     } catch (IOException ioex) {
       System.err.print("Server unavailable to give a response: ");
